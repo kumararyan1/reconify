f43cf5d1efe43edbad052b95c16a6b07513e9336
Kumar Aryan Singh <aryan21468@iiitd.ac.in>
2026-03-11 22:33:57 +0530

    Use relative path for script tag and simplify Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/reconify/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'src/main.tsx',
-      },
-    },
-  },
   server: {
     port: 3000,
     open: true,
